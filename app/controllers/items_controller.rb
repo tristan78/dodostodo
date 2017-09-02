2621f72c600191394a5f3418b9925eaafacd9b83
Tristan <tristan78@gmx.de>
2017-09-02 20:07:39 +0000

    bootstrap simpleform installiert und aufgaben struktur erstellt

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
     def index
+        @items = Item.all.order("created_at DESC")
+    end
+    
+    def new
+        @item = Item.new
+    end
+    
+    def create
+        @item = Item.new(item_params)
+        if @item.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+    
+    private
+    
+    def item_params
+        params.require(:item).permit(:title, :description)
     end
 end
